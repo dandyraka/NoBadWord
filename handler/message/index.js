43a8ad9a851a9a46523d92118bc7a3df437c1fb0
dandyraka <dandyraka13@gmail.com>
2020-09-15 13:06:56 +0700

    reset klasemen, fix save json

--- handler/message/index.js
+++ handler/message/index.js
@@ -11,41 +11,55 @@ const db = low(db_group)
 moment.tz.setDefault('Asia/Jakarta').locale('id')
 db.defaults({ group: []}).write()
 
+function formatin(duit){
+    let	reverse = duit.toString().split('').reverse().join('');
+    let ribuan = reverse.match(/\d{1,3}/g);
+    ribuan = ribuan.join('.').split('').reverse().join('');
+    return ribuan;
+}
+
+const inArray = (needle, haystack) => {
+    let length = haystack.length;
+    for(let i = 0; i < length; i++) {
+        if(haystack[i].id == needle) return i;
+    }
+    return false;
+}
+
 module.exports = msgHandler = async (client, message) => {
     try {
-        const { type, id, from, t, sender, isGroupMsg, chat, caption, isMedia, mimetype, quotedMsg, quotedMsgObj, mentionedJidList } = message
+        const { type, id, from, t, sender, isGroupMsg, chat, caption } = message
         let { body } = message
         const { name, formattedTitle } = chat
         let { pushname, verifiedName, formattedName } = sender
         pushname = pushname || verifiedName || formattedName // verifiedName is the name of someone who uses a business account
         if (pushname == undefined || pushname.trim() == '') console.log(sender)
-        const botNumber = await client.getHostNumber() + '@c.us'
         const groupId = isGroupMsg ? chat.groupMetadata.id : ''
         const groupAdmins = isGroupMsg ? await client.getGroupAdmins(groupId) : ''
-        const groupMembers = isGroupMsg ? await client.getGroupMembersId(groupId) : ''
         const isGroupAdmins = groupAdmins.includes(sender.id) || false
-        const isBotGroupAdmins = groupAdmins.includes(botNumber) || false
+        const chats = (type === 'chat') ? body : (type === 'image' || type === 'video') ? caption : ''
         const pengirim = sender.id;
+        const isKasar = await cariKasar(chats)
 
         const prefix = '#'
         body = (type === 'chat' && body.startsWith(prefix)) ? body : ((type === 'image' && caption) && caption.startsWith(prefix)) ? caption : ''
-        const chats = message.body
         const command = body.slice(prefix.length).trim().split(/ +/).shift().toLowerCase()
         const args = body.slice(prefix.length).trim().split(/ +/).slice(1)
         const isCmd = body.startsWith(prefix)
 
-        //if (!isCmd && !isGroupMsg) { return console.log('[RECV]', color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), 'Message from', color(pushname)) }
-        //if (!isCmd && isGroupMsg) { return console.log('[RECV]', color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), 'Message from', color(pushname), 'in', color(name || formattedTitle)) }
+        if(!isCmd && isKasar && isGroupMsg) { console.log(color('[BADWORD]'), color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), 'from', color(pushname), 'in', color(name || formattedTitle)) }
         if (isCmd && !isGroupMsg) { console.log(color('[EXEC]'), color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname)) }
         if (isCmd && isGroupMsg) { console.log(color('[EXEC]'), color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`), 'from', color(pushname), 'in', color(name || formattedTitle)) }
-
         switch (command) {
             case 'reset':
                 if(isGroupMsg){
-                    if(groupAdmins.includes(pengirim)){
-                        await client.sendText(from, "Ya, ini adalah reset")
+                    if(isGroupAdmins){
+                        const reset = db.get('group').find({ id: groupId }).assign({ members: []}).write()
+                        if(reset){
+                            await client.sendText(from, "Klasemen telah direset")
+                        }
                     } else {
-                        await client.sendText(from, "Klasemen dapat direset oleh Admin Grup.")
+                        await client.sendText(from, "Klasemen hanya dapat direset oleh Admin Grup.")
                     }
                 } else {
                     await client.sendText(from, "Hanya dapat dilakukan dalam Grup.")
@@ -58,8 +72,7 @@ module.exports = msgHandler = async (client, message) => {
                     let textKlas = "*Klasemen Denda Sementara*\n"
                     let i = 1;
                     urut.forEach((klsmn) => {
-                        textKlas += i+". @"+klsmn.id.replace('@c.us', '')+" ➤ "+klsmn.denda+"\n"
-                        //console.log(element.denda)
+                        textKlas += i+". @"+klsmn.id.replace('@c.us', '')+" ➤ Rp"+formatin(klsmn.denda)+"\n"
                         i++
                     });
                     await client.sendTextWithMentions(from, textKlas)
@@ -68,21 +81,11 @@ module.exports = msgHandler = async (client, message) => {
                 }
                 break
             default:
-                console.log(color('[ERROR]', 'red'), color(moment(t * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), 'Unregistered Command from', color(pushname))
                 break
-        }
-
-        const inArray = (needle, haystack) => {
-            let length = haystack.length;
-            for(let i = 0; i < length; i++) {
-                if(haystack[i].id == needle) return i;
-            }
-            return false;
         }
 
         if(!isCmd && isGroupMsg){
             const find = db.get('group').find({ id: groupId }).value()
-            const isKasar = await cariKasar(chats)
             if(find && find.id === groupId){
                 const cekuser = db.get('group').filter({id: groupId}).map('members').value()[0]
                 const isIn = inArray(pengirim, cekuser)
@@ -90,19 +93,32 @@ module.exports = msgHandler = async (client, message) => {
                     if(isKasar){
                         const denda = db.get('group').filter({id: groupId}).map('members['+isIn+']').find({ id: pengirim }).update('denda', n => n + 5000).write()
                         if(denda){
-                            await client.reply(from, "Jangan badword bodoh\nDenda +5000\nTotal : Rp."+denda.denda, id)
+                            await client.reply(from, "Jangan badword bodoh\nDenda +5000\nTotal : Rp"+formatin(denda.denda), id)
                         }
-                    } else {
-                        console.log("ganemu kata kasar")
                     }
                 } else {
-                    const getMembers = db.get('group').filter({id: groupId}).map('members[0]').push( {id: pengirim, denda: 0} ).value()
-                    db.get('group').find({ id: groupId }).set('members', getMembers).write()
+                    const cekMember = db.get('group').filter({id: groupId}).map('members').value()[0]
+                    if(cekMember.length === 0){
+                        if(isKasar){
+                            db.get('group').find({ id: groupId }).set('members', [{id: pengirim, denda: 5000}]).write()
+                        } else {
+                            db.get('group').find({ id: groupId }).set('members', [{id: pengirim, denda: 0}]).write()
+                        }
+                    } else {
+                        let upd;
+                        if(isKasar){
+                            upd = db.get('group').filter({id: groupId}).map('members[0]').push( {id: pengirim, denda: 5000} ).value()
+                        } else {
+                            upd = db.get('group').filter({id: groupId}).map('members[0]').push( {id: pengirim, denda: 0} ).value()
+                        }
+                        
+                        db.get('group').find({ id: groupId }).set('members', upd).write()
+                    }
                 }
             } else {
                 if(isKasar){
                     db.get('group').push({ id: groupId, members: [{id: pengirim, denda: 5000}] }).write()
-                    await client.reply(from, "Jangan badword bodoh\nDenda +5000\nTotal : Rp.5000", id)
+                    await client.reply(from, "Jangan badword bodoh\nDenda +5000\nTotal : Rp5.000", id)
                 } else {
                     db.get('group').push({ id: groupId, members: [{id: pengirim, denda: 0}] }).write()
                 }
